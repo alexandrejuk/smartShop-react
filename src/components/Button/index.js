b7f48ada6d3ebe04c3dba82f517a900422fec242
alexandrejuk <ale_santos.soares@hotmail.com>
2018-08-10 18:56:44 -0300

    component: add  button

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './index.css'
+
+export default class Button extends Component {
+  render() { 
+    const { text } = this.props;
+    return (<button class="button">{ text }</button>)
+  }
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+}
