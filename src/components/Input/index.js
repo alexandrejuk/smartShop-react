c793ec48d2a5eddcd98f89ec92c975f760bd2901
alexandrejuk <ale_santos.soares@hotmail.com>
2018-08-10 18:57:23 -0300

    component: add input

---
+++ src/components/Input/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './index.css'
+
+export default class Input extends Component {
+  render() { 
+    const { name, sizeInput, text, type } = this.props;
+    return ( 
+      <div className="inputWrapper">
+        <label className="label">{text}</label>
+        <input className={`input-group ${ sizeInput }`} name={name} type={type} />
+      </div>
+     )
+  }
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,  
+  sizeInput: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+}
