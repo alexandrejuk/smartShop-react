41ae272ae8bfd4d6b5992293d8f39f66b3e6c508
alexandrejuk <ale_santos.soares@hotmail.com>
2018-08-10 18:56:57 -0300

    component: add card

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './index.css'
+
+export default class Card extends Component {
+  render() { 
+    return (<div class="card"></div>)
+  }
+}
+
+Card.propTypes = {
+}
