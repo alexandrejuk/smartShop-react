e46a5d0acfb31ed5c58ae92df6c27fae1f096b32
alexandrejuk <ale_santos.soares@hotmail.com>
2018-08-10 19:26:56 -0300

    setup: style initial to content page

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import './index.css'
 
 //Pages
 import Home from '../../pages/Home'
 import Products from '../../pages/Products'
 
 const Main = () => (
-  <main>
-    <Switch>
-     <Route exact path='/' component={Home}/>
-     <Route path='/products' component={Products}/>
-    </Switch>
-  </main>
+  <div className="container">
+    <main className="main">
+      <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/products' component={Products}/>
+      </Switch>
+    </main>
+  </div>
 )
 
 export default Main
---
+++ src/pages/Products/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+  render() { 
+    return (<h1>I'm a Products page!</h1>);
+  }
+}
+ 
+export default Products;
